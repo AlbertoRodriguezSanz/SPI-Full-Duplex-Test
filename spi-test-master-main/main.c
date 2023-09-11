3798379a38a7ec24fee04cdf189b0f748e8c14ac
Alberto Rodríguez Sanz <95371514+AlbertoRodriguezSanz@users.noreply.github.com>
2023-09-11 12:22:39 +0200

    Update main.c

--- spi-test-master-main/main.c
+++ spi-test-master-main/main.c
@@ -51,23 +51,10 @@
 void main(void)
 {
     // Initialize the device
-    SYSTEM_Initialize();
-
-    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
-    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global Interrupts
-    // Use the following macros to:
-
-    // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
-
-    // Disable the Global Interrupts
-    //INTERRUPT_GlobalInterruptDisable();
-
-    
+    SYSTEM_Initialize();    
     PORTCbits.RC2 = 0; 
     PORTAbits.RA5 = 1; //SS start high (no communication)
-    
-    //SPI_Master_test();
+  
     while (1)
     {
         // Add your application code
